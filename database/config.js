1dfc7c3639bfcc3770a7e6f339d2d53721c392c6
EstebanRodGuerrero <esteban05.rguerrero@gmail.com>
2021-09-28 17:06:46 -0300

    mongo atlas env

--- database/config.js
+++ database/config.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
     try {
-        await mongoose.connect( process.env.MONGODB_CNN , { // Url que tenemos en .env
+        await mongoose.connect( process.env.MONGODB_CNN_ATLAS , { // Url que tenemos en .env
 
             useNewUrlParser: true,
             useUnifiedTopology: true
