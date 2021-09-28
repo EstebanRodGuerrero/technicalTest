2776ddc4a5cac9c474b922b4b093c2ff015cabfd
EstebanRodGuerrero <esteban05.rguerrero@gmail.com>
2021-09-28 17:30:13 -0300

    API REST technical test V1.1

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,13 +2,20 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { login } = require('../controllers/auth-controller');
+const { validarCampos } = require('../middlewares');
 
 
 
 const router = Router(); 
 
 
-    router.post( '/' , login )
+    router.post( '/' , 
+        [
+            check('username'),
+            check('password' , 'La contraseña es obligatoria').not().isEmpty(),
+            validarCampos
+        ] ,
+           login )
 
 
 
